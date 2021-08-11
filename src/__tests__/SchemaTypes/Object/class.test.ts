cc1f14dcec4f649816f8a5272742d502cf423f34
Aryaman1706 <agrover1_be19@thapar.edu>
2021-08-11 16:50:18 +0530

    :white_check_mark: test(ObjectSchema): added tests for onlyKeys and onlySupplied checks and minor refactor

--- src/__tests__/SchemaTypes/Object/class.test.ts
+++ src/__tests__/SchemaTypes/Object/class.test.ts
@@ -197,7 +197,7 @@ describe("Test ObjectSchemaType", () => {
     expect(errors).toEqual([]);
   });
 
-  it("returns no error if testObj is a nested object and keys are missing in the nested object", () => {
+  it("returns error if testObj is a nested object and keys are missing in the nested object", () => {
     type IObjectSchema = {
       name: {
         firstName: string;
@@ -232,5 +232,116 @@ describe("Test ObjectSchemaType", () => {
     });
     expect(errors[0]).toBeDefined();
     expect(errors[0]!.error).toEqual("firstName is required");
+  });
+
+  it("adds default value for default keys", () => {
+    type Address = {
+      city: string;
+      state: string;
+    };
+
+    type User = {
+      name: string;
+      address: Address;
+    };
+
+    const schema = SchemaTypes.object<User>().keys({
+      name: SchemaTypes.string().default("Username"),
+      address: SchemaTypes.object<Address>().keys({
+        city: SchemaTypes.string().required(),
+        state: SchemaTypes.string().default("Not Mentioned"),
+      }),
+    });
+
+    const testObj = {
+      address: {
+        city: "Delhi",
+      },
+    };
+
+    const { valid, value, errors } = schema.validate(testObj);
+
+    expect(valid).toBe(true);
+    expect(value).toEqual({
+      name: "Username",
+      address: {
+        city: "Delhi",
+        state: "Not Mentioned",
+      },
+    });
+    expect(errors).toEqual([]);
+  });
+
+  it("checks only the supplied keys", () => {
+    type Address = {
+      city: string;
+      state: string;
+    };
+
+    type User = {
+      name: string;
+      address: Address;
+    };
+
+    const schema = SchemaTypes.object<User>().keys({
+      name: SchemaTypes.string().default("Username"),
+      address: SchemaTypes.object<Address>().keys({
+        city: SchemaTypes.string().minLength(10).required(),
+        state: SchemaTypes.string().required(),
+      }),
+    });
+
+    const testObj = {
+      address: {
+        city: "Delhi",
+      },
+    };
+
+    const { valid, value, errors } = schema.validate(testObj, undefined, {
+      onlySupplied: true,
+    });
+
+    expect(valid).toBe(false);
+    expect(value).toEqual({
+      address: {
+        city: "Delhi",
+      },
+    });
+    expect(errors.length).toBe(1);
+    expect(errors[0]).toEqual({
+      error: "minimum allowed length for city is 10",
+      errorType: "String/Length",
+    });
+  });
+
+  it("only checks keys and ignores the pattern check", () => {
+    type IObjectSchema = {
+      name: string;
+    };
+
+    const objectSchema = SchemaTypes.object<IObjectSchema>()
+      .keys({
+        name: SchemaTypes.string().required(),
+      })
+      .pattern([SchemaTypes.string(), SchemaTypes.number()]);
+
+    const testObj = {
+      name: "firefly",
+      age: 10,
+    };
+
+    const { valid, value, errors } = objectSchema.validate(testObj, undefined, {
+      onlyKeys: true,
+    });
+
+    expect(valid).toBe(false);
+    expect(value).toEqual({
+      name: "firefly",
+    });
+    expect(errors.length).toBe(1);
+    expect(errors[0]).toEqual({
+      error: "age is not allowed",
+      errorType: "Object/InvalidKeys",
+    });
   });
 });
