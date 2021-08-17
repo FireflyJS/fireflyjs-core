44a4970da8ec6e679cb7935794b733a7f26f845f
aniketbiswas21 <aniket.biswas75@gmail.com>
2021-08-18 01:57:17 +0530

    :sparkles: docs(Connection): Added documentation for Connection class

--- src/Connection/types/ModelPOJO.ts
+++ src/Connection/types/ModelPOJO.ts
@@ -1,5 +1,8 @@
 import Model from "../../Model/class";
 
+/**
+ * Object containing model name as keys and Model instance as values
+ */
 type ModelPOJO = { [k: string]: Model };
 
 export { ModelPOJO };
